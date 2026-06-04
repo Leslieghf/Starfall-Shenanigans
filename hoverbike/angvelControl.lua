767f40b7fac5ed7fb9e965222f4f92b8fd9ec3e4
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2026-06-04 14:14:08 +0200

    dings

--- hoverbike/angvelControl.lua
+++ hoverbike/angvelControl.lua
@@ -1,20 +1,13 @@
---@include utils.lua
-
-local Utils = require("utils.lua")
-
 local AngVelControl = {}
-AngVelControl.DAMPING_FACTOR = 0.01
+AngVelControl.DAMPING_FACTOR = 1.0
 
 function AngVelControl.getRotationalDampingTorque(ent)
     local angVel = ent:getAngleVelocity()
-    local inertia = ent:getInertia()
-
-    return inertia * angVel * -AngVelControl.DAMPING_FACTOR
+    return angVel * -AngVelControl.DAMPING_FACTOR
 end
 
 function AngVelControl.applyTotalTorque(ent)
     local torque = AngVelControl.getRotationalDampingTorque(ent)
-    Utils.debugPrint("Applying torque: '" .. tostring(torque) .. "'.")
     ent:applyTorque(torque)
 end
 
